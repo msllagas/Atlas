b06372801a6f3268874e457b4c527ebd5205e569
William <blazerx1234@gmail.com>
2022-01-02 11:57:44 +0800

    login form

--- Atlas/MainWindow.xaml.cs
+++ Atlas/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+using System.Configuration;
+using System.Security;
 
 namespace Atlas
 {
@@ -20,9 +23,105 @@ namespace Atlas
     /// </summary>
     public partial class MainWindow : Window
     {
+        private MySqlConnection conn;
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
+
         public MainWindow()
         {
+            server = "localhost";
+            database = "atlas";
+            uid = "root";
+            password = "";
+
+            string connString;
+            connString = $"SERVER={server};DATABASE={database};UID={uid};PASSWORD={password};";
+            conn = new MySqlConnection(connString);
             InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            string user = txtUsername.Text;
+            string pass = txtPassword.Password;
+
+            if(IsLogin(user, pass))
+            {
+                MessageBox.Show($"Welcome {user}!");
+            }
+            else
+            {
+                MessageBox.Show($"User or pass is incorrect!");
+            }
+        }
+
+        public bool IsLogin(string user, string pass)
+        {
+            string query = $"SELECT * FROM users WHERE Username='{user}' AND Password='{pass}';";
+            try
+            {
+                if(OpenConnection())
+                {
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+                    MySqlDataReader reader = cmd.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        reader.Close();
+                        conn.Close();
+                        return true;
+                    }
+                    else
+                    {
+                        reader.Close();
+                        conn.Close();
+                        return false;
+                    }
+                }
+                else
+                {
+                    conn.Close();
+                    return false;
+                }
+
+            } catch (Exception ex)
+            {
+                conn.Close();
+                return false;
+            }
+        }
+        
+        private bool OpenConnection()
+        {
+            try
+            {
+                conn.Open();
+                return true;
+            }
+            catch(MySqlException ex)
+            {
+                switch (ex.Number)
+                {
+                    case 0:
+                        MessageBox.Show("Connection to server failed");
+                        break;
+                    case 1045:
+                        MessageBox.Show("Server user or password is incorrect");
+                        break;
+                }
+                return false;
+            }
+        }
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            //if (e.ChangedButton == MouseButton.Left)
+            //    this.DragMove();
+        }
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
         }
     }
 }
