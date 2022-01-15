d9b0b37454d9f54a6859bb66406b10a436c9822f
dyesikapey <jessicafayev026@gmail.com>
2022-01-15 20:46:05 +0800

    edit AddDelivery, code for dataGrid/table items

--- Atlas/Pages/AddDelivery.xaml.cs
+++ Atlas/Pages/AddDelivery.xaml.cs
@@ -10,6 +10,13 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
 
 namespace Atlas.Pages
 {
@@ -21,12 +28,103 @@ namespace Atlas.Pages
         public AddDelivery()
         {
             InitializeComponent();
+            addTable.ItemsSource = Products.GetProducts();
         }
 
+        public class Products :INotifyPropertyChanged
+        {
+            private string prodID;
+            public string ProductID
+            {
+                get { return prodID; }
+                set
+                {
+                    prodID = value;
+                    RaiseProperChanged();
+                }
+            }
+
+            private string name;
+            public string ProductName
+            {
+                get { return name; }
+                set
+                {
+                    name = value;
+                    RaiseProperChanged();
+                }
+            }           
+
+            private int quantity;
+            public int Quantity
+            {
+                get { return quantity; }
+                set
+                {
+                    quantity = value;
+                    RaiseProperChanged();
+                }
+            }
+
+            private string unit;
+            public string UnitCost
+            {
+                get { return unit; }
+                set
+                {
+                    unit = value;
+                    RaiseProperChanged();
+                }
+            }
+
+            private string total;
+            public string TotalCost
+            {
+                get { return total; }
+                set
+                {
+                    total = value;
+                    RaiseProperChanged();
+                }
+            }
+
+            public static ObservableCollection<Products> GetProducts()
+            {
+                var item = new ObservableCollection<Products>();
+                item.Add(new Products()
+                {
+                    ProductID = "001",
+                    ProductName = "Gtech",
+                    Quantity = 1,
+                    UnitCost = "P70.00",
+                    TotalCost = "P70.00"
+                });
+                item.Add(new Products()
+                {
+                    ProductID = "002",
+                    ProductName = "Mongol 08",
+                    Quantity = 11,
+                    UnitCost = "P70.00",
+                    TotalCost = "P70.00"
+                });
+                return item;
+            }
+            public event PropertyChangedEventHandler PropertyChanged;
+            private void RaiseProperChanged([CallerMemberName] string caller = "")
+            {
+
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs(caller));
+                }
+            }
+
+        }
         private void cancel_btn_Click(object sender, RoutedEventArgs e)
         {
             Delivery gotopage = new Delivery();
             this.NavigationService.Navigate(gotopage);
         }
+
     }
 }
