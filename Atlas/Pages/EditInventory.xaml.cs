e846647414b5729d32dda2a4cee3bbfe3ca8510b
dyesikapey <jessicafayev026@gmail.com>
2022-01-15 17:40:52 +0800

    EditInventory page Added

---
+++ Atlas/Pages/EditInventory.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Atlas.Pages
+{
+    /// <summary>
+    /// Interaction logic for EditInventory.xaml
+    /// </summary>
+    public partial class EditInventory : Page
+    {
+        public EditInventory()
+        {
+            InitializeComponent();
+        }
+
+        private void cancel_Click(object sender, RoutedEventArgs e)
+        {
+            Inventory gotopage = new Inventory();
+            this.NavigationService.Navigate(gotopage);
+        }
+
+        private void edit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
